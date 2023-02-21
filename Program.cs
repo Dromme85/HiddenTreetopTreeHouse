aef4037e684a97aa8a0d4f04f18866dd6ff09800
Dromme85 <74305385+Dromme85@users.noreply.github.com>
2023-02-21 10:46:10 +0100

    Part 2

--- Program.cs
+++ Program.cs
@@ -3,11 +3,50 @@ namespace HiddenTreetopTreeHouse
 {
 	static class Program
 	{
+		public static GridHandler gh { get; private set; }
+
 		static void Main()
 		{
-			GridHandler tg = new("../../../treegrid.txt");
+			gh = new("../../../treegrid.txt");
 
-			tg.PrintResult();
+			PrintResult();
+		}
+
+		/// <summary>
+		/// Simple visualization of the tree grid,
+		/// displaying the height of visible trees
+		/// and hiding trees that's not visible.
+		/// </summary>
+		static void PrintResult()
+		{
+			int visibleTrees = gh.CalculateVisibleTrees();
+			gh.CalculateScenicScores();
+			long highestScenicScore = 1;
+			int[] highScorePos = new int[2];
+
+			for (int y = 0; y < gh.Grid.GetLength(0); y++)
+			{
+				for (int x = 0; x < gh.Grid.GetLength(1); x++)
+				{
+					if (gh.Grid[x, y].Visible)
+					{
+						Console.Write(gh.Grid[x, y].Data + " ");
+					}
+					else Console.Write("  ");
+
+					if (gh.Grid[x, y].ScenicScore > highestScenicScore)
+					{
+						highestScenicScore = gh.Grid[x, y].ScenicScore;
+						highScorePos = new int[2] { x, y };
+					}
+				}
+				Console.WriteLine();
+			}
+			var s = gh.Grid[highScorePos[0], highScorePos[1]].Scores;
+
+			Console.WriteLine($"\nThere are {visibleTrees} visible trees out of {gh.Grid.Length}.");
+			Console.WriteLine($"\nThe spot with the highest score is {highestScenicScore} ({s[0]} * {s[1]} * {s[2]} * {s[3]})" +
+				$" and can be found at {highScorePos[0]}, {highScorePos[1]}.");
 		}
 	}
 
--- TreeGrid.cs
+++ TreeGrid.cs
@@ -9,19 +9,25 @@ using System.Threading.Tasks;
 
 namespace HiddenTreetopTreeHouse
 {
+	// The extra attributes is a workaround to be able to declare size of an array
+	// https://stackoverflow.com/questions/4123314/how-i-can-declare-arrays-in-struct
 	[StructLayout(LayoutKind.Sequential)]
 	struct TreeGrid
 	{
 		public byte Data;
 		public bool Visible;
+		public long ScenicScore;
 		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
 		public bool[] Direction;
+		public int[] Scores;
 
 		public TreeGrid()
 		{
 			Data = 0;
 			Visible = false;
+			ScenicScore = 0;
 			Direction = new bool[4];
+			Scores = new int[4];
 		}
 	}
 
@@ -39,62 +45,58 @@ namespace HiddenTreetopTreeHouse
 			}
 			catch
 			{
-				// if any errors, assign some test data.
-				gs = new string[] { "30373", "25512", "65332", "33549", "35390" };
+				// If any errors while reading the file, use some test data.
+				gs = new string[]
+				{
+					"30373", 
+					"25512",
+					"65332", 
+					"33549", 
+					"35390" 
+				};
 			}
 
 			Grid = new TreeGrid[gs[0].Length, gs.Length];
-			for (int x = 0; x < gs.Length; x++)
+			for (int y = 0; y < gs.Length; y++)
 			{
-				for (int y = 0; y < gs[x].Length; y++)
+				for (int x = 0; x < gs[y].Length; x++)
 				{
 					Grid[x, y] = new TreeGrid();
 				}
 			}
 
-			for (int x = 0; x < gs.Length; x++)
+			for (int y = 0; y < gs.Length; y++)
 			{
-				for (int y = 0; y < gs[x].Length; y++)
+				for (int x = 0; x < gs[y].Length; x++)
 				{
-					byte.TryParse(gs[x][y].ToString(), out byte d);
+					byte.TryParse(gs[y][x].ToString(), out byte d);
 					Grid[x, y].Data = d;
-					if (x == 0 || x == gs.Length - 1 || y == 0 || y == gs[gs.Length - 1].Length - 1)
+
+					// We already know the edge is visible,
+					// lets set it to visible and what direction that is visible
+					if (y == 0 || y == gs.Length - 1 || x == 0 || x == gs[gs.Length - 1].Length - 1)
 					{
 						Grid[x, y].Visible = true;
-						if (x == 0) Grid[x, y].Direction[0] = true;
-						if (x == gs.Length - 1) Grid[x, y].Direction[2] = true;
-						if (y == 0) Grid[x, y].Direction[3] = true;
-						if (y == gs[gs.Length - 1].Length - 1) Grid[x, y].Direction[1] = true;
+						if (y == 0) Grid[x, y].Direction[0] = true;
+						if (y == gs.Length - 1) Grid[x, y].Direction[2] = true;
+						if (x == 0) Grid[x, y].Direction[3] = true;
+						if (x == gs[gs.Length - 1].Length - 1) Grid[x, y].Direction[1] = true;
 					}
 				}
 			}
 		}
 
-		public void PrintResult()
-		{
-			var visibleTrees = CalculateVisibleTrees();
-
-			for (int x = 0; x < Grid.GetLength(0); x++)
-			{
-				for (int y = 0; y < Grid.GetLength(1); y++)
-				{
-					if (Grid[x, y].Visible)
-						Console.Write(Grid[x, y].Data + " ");
-					else Console.Write("  ");
-				}
-				Console.WriteLine();
-			}
-
-			Console.WriteLine($"There are {visibleTrees} visible trees out of {Grid.Length}.");
-		}
-
+		/// <summary>
+		/// Loops through the grid and checks visibility of the trees.
+		/// </summary>
+		/// <returns>The number of trees visible</returns>
 		public int CalculateVisibleTrees()
 		{
 			int val = Grid.GetLength(0) * 2 + Grid.GetLength(1) * 2 - 4;
 
-			for (int x = 1; x < Grid.GetLength(0) - 1; x++)
+			for (int y = 1; y < Grid.GetLength(0) - 1; y++)
 			{
-				for (int y = 1; y < Grid.GetLength(1) - 1; y++)
+				for (int x = 1; x < Grid.GetLength(1) - 1; x++)
 				{
 					Grid[x, y].Direction[0] = CheckDirection(0, x, y);
 					Grid[x, y].Direction[1] = CheckDirection(1, x, y);
@@ -112,6 +114,14 @@ namespace HiddenTreetopTreeHouse
 			return val;
 		}
 
+		/// <summary>
+		/// Recursive method to check visibility of a tree in a given direction.
+		/// </summary>
+		/// <param name="d">Direction (0 = up, 1 = right, 2 = down, 3 = left)</param>
+		/// <param name="x">Horisontal position</param>
+		/// <param name="y">Vertical position</param>
+		/// <param name="o">Offset position from x, y</param>
+		/// <returns>True if tree on x, y is visible</returns>
 		private bool CheckDirection(int d, int x, int y, int o = 0)
 		{
 			o++;
@@ -119,34 +129,114 @@ namespace HiddenTreetopTreeHouse
 			switch (d)
 			{
 				case 0: // Up
-					if (Grid[x, y].Data > Grid[x - o, y].Data)
-					{
-						if (x - o == 0) return true;
-						return CheckDirection(d, x, y, o);
-					}
-					return false;
-				case 1: // Right
-					if (Grid[x, y].Data > Grid[x, y + o].Data)
-					{
-						if (y + o == Grid.GetLength(1) - 1) return true;
-						return CheckDirection(d, x, y, o);
-					}
-					return false;
-				case 2: // Down
-					if (Grid[x, y].Data > Grid[x + o, y].Data)
-					{
-						if (x + o == Grid.GetLength(0) - 1) return true;
-						return CheckDirection(d, x, y, o);
-					}
-					return false;
-				case 3: // Left
 					if (Grid[x, y].Data > Grid[x, y - o].Data)
 					{
 						if (y - o == 0) return true;
 						return CheckDirection(d, x, y, o);
 					}
 					return false;
+
+				case 1: // Right
+					if (Grid[x, y].Data > Grid[x + o, y].Data)
+					{
+						if (x + o == Grid.GetLength(1) - 1) return true;
+						return CheckDirection(d, x, y, o);
+					}
+					return false;
+
+				case 2: // Down
+					if (Grid[x, y].Data > Grid[x, y + o].Data)
+					{
+						if (y + o == Grid.GetLength(0) - 1) return true;
+						return CheckDirection(d, x, y, o);
+					}
+					return false;
+
+				case 3: // Left
+					if (Grid[x, y].Data > Grid[x - o, y].Data)
+					{
+						if (x - o == 0) return true;
+						return CheckDirection(d, x, y, o);
+					}
+					return false;
+
 				default: return false;
+			}
+		}
+
+		/// <summary>
+		/// Loops through the grid and calculates the scenic score of the trees.
+		/// </summary>
+		public void CalculateScenicScores()
+		{
+			// Lets assume the edge will not have the highest scenic score and skip those trees.
+			for (int y = 1; y < Grid.GetLength(0) - 1; y++)
+			{
+				for (int x = 1; x < Grid.GetLength(1) - 1; x++)
+				{
+					// Only visible trees need to be calculated.
+					if (Grid[x, y].Visible)
+					{
+						Grid[x, y].Scores[0] = CheckDirectionalScore(0, x, y);
+						Grid[x, y].Scores[1] = CheckDirectionalScore(1, x, y);
+						Grid[x, y].Scores[2] = CheckDirectionalScore(2, x, y);
+						Grid[x, y].Scores[3] = CheckDirectionalScore(3, x, y);
+
+						long product = 1;
+						foreach (int value in Grid[x, y].Scores)
+						{
+							if (value > 0) product *= value;
+						}
+
+						Grid[x, y].ScenicScore = product;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Recursive method to calculate the scenic score of a tree in a given direction.
+		/// </summary>
+		/// <param name="d">Direction (0 = up, 1 = right, 2 = down, 3 = left)</param>
+		/// <param name="x">Horisontal position</param>
+		/// <param name="y">Vertical position</param>
+		/// <param name="o">Offset position from x, y</param>
+		/// <returns>The score of the given direction</returns>
+		private int CheckDirectionalScore(int d, int x, int y, int o = 0)
+		{
+			o++;
+
+			switch (d)
+			{
+				case 0: // Up
+					Grid[x, y].Scores[d]++;
+					if (Grid[x, y].Data > Grid[x, y - o].Data
+						&& y - o > 0)
+						CheckDirectionalScore(d, x, y, o);
+					return Grid[x, y].Scores[d];
+
+				case 1: // Right
+					Grid[x, y].Scores[d]++;
+					if (Grid[x, y].Data > Grid[x + o, y].Data
+						&& x + o < Grid.GetLength(0) - 1)
+						return CheckDirectionalScore(d, x, y, o);
+					return Grid[x, y].Scores[d];
+
+				case 2: // Down
+					Grid[x, y].Scores[d]++;
+					if (Grid[x, y].Data > Grid[x, y + o].Data
+						&& y + o < Grid.GetLength(1) - 1)
+						return CheckDirectionalScore(d, x, y, o);
+					return Grid[x, y].Scores[d];
+
+				case 3: // Left
+					Grid[x, y].Scores[d]++;
+					if (Grid[x, y].Data > Grid[x - o, y].Data
+						&& x - o > 0)
+						return CheckDirectionalScore(d, x, y, o);
+					return Grid[x, y].Scores[d];
+
+				default: return 0;
 			}
 		}
 	}
